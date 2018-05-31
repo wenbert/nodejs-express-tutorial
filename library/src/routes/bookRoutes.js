efc5ebb7fa3e9e4c539a7e2cf3043f090056c8c4
wenbert <wenbert@gmail.com>
2018-05-31 21:06:12 +1200

    bookRoutes

--- library/src/routes/bookRoutes.js
+++ library/src/routes/bookRoutes.js
@@ -2,55 +2,57 @@ const express = require('express');
 
 const bookRouter = express.Router();
 
-function router(nav) {
-  const books = [
-    {
-      title: 'The Time Machine',
-      genre: 'Science Fiction',
-      author: 'H. G. Wells',
-      read: false,
-    },
-    {
-      title: 'The Dark World',
-      genre: 'Fantasy',
-      author: 'Henry Kuttner',
-      read: false,
-    },
-  ];
+const books = [
+  {
+    title: 'The Time Machine',
+    genre: 'Science Fiction',
+    author: 'H. G. Wells',
+    read: false,
+  },
+  {
+    title: 'The Dark World',
+    genre: 'Fantasy',
+    author: 'Henry Kuttner',
+    read: false,
+  },
+];
 
-  bookRouter.route('/')
-    .get((req, res) => {
-      res.render(
-        'books',
-        {
-          title: 'MyLibrary',
-          nav,
-          books,
-        },
-      );
-    });
+bookRouter.route('/')
+  .get((req, res) => {
+    res.render(
+      'books',
+      {
+        title: 'MyLibrary',
+        nav: [
+          { link: '/books', title: 'Books' },
+          { link: '/authors', title: 'Authors' },
+        ],
+        books,
+      },
+    );
+  });
 
-  bookRouter.route('/:id')
-    .get((req, res) => {
-      // We can do this, but ESLint advices us to use destructing
-      // const id = req.params.id;
-      const { id } = req.params;
-      res.render(
-        'book',
-        {
-          title: 'MyLibrary',
-          nav: [
-            { link: '/books', title: 'Books' },
-            { link: '/authors', title: 'Authors' },
-          ],
-          book: books[id],
-        },
+// bookRouter.route('/single')
+//   .get((req, res) => {
+//     res.render('hello single book');
+//   });
+bookRouter.route('/:id')
+  .get((req, res) => {
+    // We can do this, but ESLint advices us to use
+    // const id = req.params.id;
+    const { id } = req.params;
+    res.render(
+      'book',
+      {
+        title: 'MyLibrary',
+        nav: [
+          { link: '/books', title: 'Books' },
+          { link: '/authors', title: 'Authors' },
+        ],
+        book: books[id],
+      },
 
-      );
-    });
+    );
+  });
 
-  return bookRouter;
-}
-
-
-module.exports = router;
+module.exports = bookRouter;
