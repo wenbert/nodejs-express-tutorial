b733a678e06592623214b03ef553aa8dddecabc7
wenbert <wenbert@gmail.com>
2018-05-31 21:25:37 +1200

    fix bookRoutes

--- library/src/routes/bookRoutes.js
+++ library/src/routes/bookRoutes.js
@@ -2,57 +2,48 @@ const express = require('express');
 
 const bookRouter = express.Router();
 
-const books = [
-  {
-    title: 'The Time Machine',
-    genre: 'Science Fiction',
-    author: 'H. G. Wells',
-    read: false,
-  },
-  {
-    title: 'The Dark World',
-    genre: 'Fantasy',
-    author: 'Henry Kuttner',
-    read: false,
-  },
-];
+// The function!
+function router(nav) {
+  const books = [
+    {
+      title: 'The Time Machine', genre: 'Science Fiction', author: 'H. G. Wells', read: false,
+    },
+    {
+      title: 'The Dark World', genre: 'Fantasy', author: 'Henry Kuttner', read: false,
+    },
+  ];
 
-bookRouter.route('/')
-  .get((req, res) => {
-    res.render(
-      'books',
-      {
-        title: 'MyLibrary',
-        nav: [
-          { link: '/books', title: 'Books' },
-          { link: '/authors', title: 'Authors' },
-        ],
-        books,
-      },
-    );
-  });
+  bookRouter.route('/')
+    .get((req, res) => {
+      res.render(
+        'books',
+        {
+          title: 'MyLibrary',
+          nav,
+          books,
+        },
+      );
+    });
 
-// bookRouter.route('/single')
-//   .get((req, res) => {
-//     res.render('hello single book');
-//   });
-bookRouter.route('/:id')
-  .get((req, res) => {
-    // We can do this, but ESLint advices us to use
-    // const id = req.params.id;
-    const { id } = req.params;
-    res.render(
-      'book',
-      {
-        title: 'MyLibrary',
-        nav: [
-          { link: '/books', title: 'Books' },
-          { link: '/authors', title: 'Authors' },
-        ],
-        book: books[id],
-      },
+  bookRouter.route('/:id')
+    .get((req, res) => {
+      const { id } = req.params;
+      res.render(
+        'book',
+        {
+          title: 'MyLibrary',
+          nav: [
+            { link: '/books', title: 'Books' },
+            { link: '/authors', title: 'Authors' },
+          ],
+          book: books[id],
+        },
 
-    );
-  });
+      );
+    });
+  // Make sure we return this.
+  return bookRouter;
+}
 
-module.exports = bookRouter;
+// Then use `router` to export
+module.exports = router;
